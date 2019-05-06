1c85e30ce816d04aa706e11502ea137f35615cf7
markopuza <markopuza@gmail.com>
2019-05-06 23:20:45 +0100

    Legendre symbol

--- Mathematics/Number-theory/Legendre-symbol.py
+++ Mathematics/Number-theory/Legendre-symbol.py
@@ -1,8 +1,19 @@
 ''' Legendre symbol indicates whether a is a quadratic residue modulo p '''
 
+
+
+#########################################################################
+
 def legendre_symbol(a, p):
     ls = pow(a, (p - 1)//2, p)
     return -1 if ls == p - 1 else ls
+
+#########################################################################
+
+
+
+
+
 
 
 '''    Verification test    '''
