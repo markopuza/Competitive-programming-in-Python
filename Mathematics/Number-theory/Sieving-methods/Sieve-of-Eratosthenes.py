14ae584a479198b709a2a024c3ad2dd9bb3cb336
markopuza <markopuza@gmail.com>
2019-05-06 23:33:35 +0100

    Sieve of Eratosthenes

---
+++ Mathematics/Number-theory/Sieving-methods/Sieve-of-Eratosthenes.py
@@ -0,0 +1,14 @@
+def fast_prime_sieve(n):
+    """
+        Input n>=6, Returns a list of primes, 2 <= p <= n
+        Taken from: https://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n/
+    """
+    n += 1
+    n, correction = n-n%6+6, 2-(n%6>1)
+    sieve = [True] * (n//3)
+    for i in range(1,int(n**0.5)//3+1):
+      if sieve[i]:
+        k=3*i+1|1
+        sieve[      k*k//3      ::2*k] = [False] * ((n//6-k*k//6-1)//k+1)
+        sieve[k*(k-2*(i&1)+4)//3::2*k] = [False] * ((n//6-k*(k-2*(i&1)+4)//6-1)//k+1)
+    return [2,3] + [3*i+1|1 for i in range(1,n//3-correction) if sieve[i]]
