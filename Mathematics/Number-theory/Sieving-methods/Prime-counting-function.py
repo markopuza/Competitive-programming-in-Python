8310eaedb4efbe11d05e813107c3b2e4a5bd2cfa
markopuza <markopuza@gmail.com>
2019-05-06 23:19:02 +0100

    Prime counting function

--- Mathematics/Number-theory/Sieving-methods/Prime-counting-function.py
+++ Mathematics/Number-theory/Sieving-methods/Prime-counting-function.py
@@ -3,32 +3,32 @@
 #########################################################################
 
 def pi(n):
-    ''' Returns two arrays arr1, arr2:
-            arr1[i] = pi(i) for i <= sqrt(n)
-            arr2[i] = pi(n // i) for i <= sqrt(n)
+    ''' Returns two arrays pi1, pi2:
+            pi1[i] = pi(i) for i <= sqrt(n)
+            pi2[i] = pi(n // i) for i <= sqrt(n)
      '''
     lim = int(n**0.5)
     while lim*lim <= n: lim += 1
     lim -= 1
 
-    arr1, arr2 = [0]*(lim + 1), [0]*(lim + 1)
+    pi1, pi2 = [0]*(lim + 1), [0]*(lim + 1)
     for i in range(1, lim + 1):
-        arr1[i] = i - 1
-        arr2[i] = n//i - 1
+        pi1[i] = i - 1
+        pi2[i] = n//i - 1
 
     for i in range(2, lim + 1):
-        if arr1[i] == arr1[i - 1]:
+        if pi1[i] == pi1[i - 1]:
             continue
-        p = arr1[i - 1]
+        p = pi1[i - 1]
         for j in range(1, min(n // (i * i), lim) + 1):
             st = i * j
             if st <= lim:
-                arr2[j] -= arr2[st] - p
+                pi2[j] -= pi2[st] - p
             else:
-                arr2[j] -= arr1[n // st] - p
+                pi2[j] -= pi1[n // st] - p
         for j in range(lim, min(lim, i * i - 1), -1):
-            arr1[j] -= arr1[j // i] - p
-    return arr1, arr2
+            pi1[j] -= pi1[j // i] - p
+    return pi1, pi2
 
 #########################################################################
 
@@ -43,11 +43,11 @@ def pi(n):
 if __name__ == '__main__':
     from sympy.ntheory.generate import primepi
     n = 10**6
-    arr1, arr2 = pi(n)
-    for i in range(len(arr1)):
-        assert arr1[i] == primepi(i)
-    for i in range(1, len(arr2)):
-        assert arr2[i] == primepi(n // i)
+    pi1, pi2 = pi(n)
+    for i in range(len(pi1)):
+        assert pi1[i] == primepi(i)
+    for i in range(1, len(pi2)):
+        assert pi2[i] == primepi(n // i)
 
 
 '''
