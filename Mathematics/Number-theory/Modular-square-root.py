4e2c98de2934e8d2bdde8e1624f29759dea69f74
markopuza <markopuza@gmail.com>
2019-05-06 23:26:10 +0100

    Modular square root

---
+++ Mathematics/Number-theory/Modular-square-root.py
@@ -0,0 +1,59 @@
+''' Modular square root '''
+
+
+#########################################################################
+
+def legendre_symbol(a, p):
+    ls = pow(a, (p - 1) // 2, p)
+    return -1 if ls == p - 1 else ls
+
+def modular_sqrt(a, p):
+    if legendre_symbol(a, p) != 1:
+        return 0
+    elif a == 0:
+        return 0
+    elif p == 2:
+        return 0
+    elif p % 4 == 3:
+        return pow(a, (p + 1) // 4, p)
+    s = p - 1
+    e = 0
+    while s % 2 == 0:
+        s //= 2
+        e += 1
+    n = 2
+    while legendre_symbol(n, p) != -1:
+        n += 1
+    x = pow(a, (s + 1) // 2, p)
+    b = pow(a, s, p)
+    g = pow(n, s, p)
+    r = e
+
+    while True:
+        t = b
+        m = 0
+        for m in range(r):
+            if t == 1:
+                break
+            t = pow(t, 2, p)
+
+        if m == 0:
+            return x
+        gs = pow(g, 2 ** (r - m - 1), p)
+        g = (gs * gs) % p
+        x = (x * gs) % p
+        b = (b * g) % p
+        r = m
+
+#########################################################################
+
+
+
+
+'''    Verification test    '''
+if __name__ == "__main__":
+    for p in [17, 23, 53]:
+        for i in range(1, p):
+            r = modular_sqrt(i, p)
+            if r:
+                assert r*r%p == i
